7d9166fb0b21cdbea0533ea950c598182cddd167
Stepan Korshakov <me@ex3ndr.com>
2013-12-26 22:04:54 +0400

    Fix TS-76: Built-in known dh prime

--- app/src/main/java/org/telegram/android/core/EncryptionController.java
+++ app/src/main/java/org/telegram/android/core/EncryptionController.java
@@ -4,6 +4,7 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.os.SystemClock;
 import org.telegram.android.StelsApplication;
 import org.telegram.android.core.model.*;
 import org.telegram.android.core.model.media.TLLocalEncryptedFileLocation;
@@ -37,6 +38,11 @@ import static org.telegram.tl.StreamingUtils.readLong;
  */
 public class EncryptionController {
     private static final String TAG = "Encryption";
+
+    private static final BigInteger[] KNOWN_PRIMES = new BigInteger[]
+            {
+                    new BigInteger("C71CAEB9C6B1C9048E6C522F70F13F73980D40238E3E21C14934D037563D930F48198A0AA7C14058229493D22530F4DBFA336F6E0AC925139543AED44CCE7C3720FD51F69458705AC68CD4FE6B6B13ABDC9746512969328454F18FAF8C595F642477FE96BB2A941D5BCD1D4AC8CC49880708FA9B378E3C4F3A9060BEE67CF9A4A4A695811051907E162753B56B0F6B410DBA74D8A84B2A14B3144E0EF1284754FD17ED950D5965B4B9DD46582DB1178D169C6BC465B0D6FF9CA3928FEF5B9AE4E418FC15E83EBEA0F87FA9FF5EED70050DED2849F47BF959D956850CE929851F0D8115F635B105EE2E4E15D04B2454BF6F4FADF034B10403119CD8E3B92FCC5B", 16)
+            };
 
     private StelsApplication application;
 
@@ -394,6 +400,13 @@ public class EncryptionController {
         if (p.bitLength() != 2048) {
             throw new IOException();
         }
+
+        for (int i = 0; i < KNOWN_PRIMES.length; i++) {
+            if (KNOWN_PRIMES[i].equals(p)) {
+                return;
+            }
+        }
+
         if (!p.isProbablePrime(20)) {
             throw new IOException();
         }
@@ -450,7 +463,10 @@ public class EncryptionController {
         byte[] prime = dhConfig.getP();
         BigInteger dhPrime = loadBigInt(prime);
 
+        long start = SystemClock.uptimeMillis();
         checkDhConfig(dhPrime, dhConfig.getG());
+        Logger.d(TAG, "DH checked in " + (SystemClock.uptimeMillis() - start) + " ms");
+        start = SystemClock.uptimeMillis();
 
         BigInteger g = new BigInteger("" + dhConfig.getG());
 
@@ -467,6 +483,8 @@ public class EncryptionController {
         if (ga.compareTo(pow2) <= 0 || ga.compareTo(dhPrime.subtract(pow2)) >= 0) {
             throw new IOException();
         }
+
+        Logger.d(TAG, "GA checked in " + (SystemClock.uptimeMillis() - start) + " ms");
 
         TLAbsEncryptedChat chat = application.getApi().doRpcCall(
                 new TLRequestMessagesRequestEncryption(
