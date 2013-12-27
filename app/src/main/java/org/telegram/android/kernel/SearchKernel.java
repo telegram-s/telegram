14e5ece2d4354ed00172c676831dd703377aa9e7
Stepan Korshakov <me@ex3ndr.com>
2013-12-27 05:25:08 +0400

    Fixed search

--- app/src/main/java/org/telegram/android/kernel/SearchKernel.java
+++ app/src/main/java/org/telegram/android/kernel/SearchKernel.java
@@ -2,10 +2,12 @@ package org.telegram.android.kernel;
 
 import android.text.SpannableString;
 import org.telegram.android.StelsApplication;
+import org.telegram.android.core.ContactsSource;
 import org.telegram.android.core.model.DialogDescription;
 import org.telegram.android.core.model.PeerType;
 import org.telegram.android.core.model.SearchWireframe;
 import org.telegram.android.core.model.User;
+import org.telegram.android.core.wireframes.DialogWireframe;
 import org.telegram.android.ui.FilterMatcher;
 
 import java.util.ArrayList;
@@ -32,22 +34,22 @@ public class SearchKernel {
 
         ArrayList<SearchWireframe> wireframes = new ArrayList<SearchWireframe>();
 
-//        for (DialogDescription description : application.getDialogSource().getViewSource().getCurrentWorkingSet()) {
-//            long id = description.getPeerId() * 2 + description.getPeerType();
-//            if (founded.contains(id)) {
-//                continue;
-//            }
-//            if (matcher.isMatched(description.getTitle())) {
-//                founded.add(id);
-//                SpannableString spannableString = new SpannableString(description.getTitle());
-//                if (description.getPeerType() == PeerType.PEER_USER) {
-//                    User usr = application.getEngine().getUser(description.getPeerId());
-//                    wireframes.add(new SearchWireframe(description.getPeerId(), description.getPeerType(), description.getTitle(), spannableString, description.getPhoto(), usr.getStatus(), 0));
-//                } else {
-//                    wireframes.add(new SearchWireframe(description.getPeerId(), description.getPeerType(), description.getTitle(), spannableString, description.getPhoto(), null, description.getParticipantsCount()));
-//                }
-//            }
-//        }
+        for (DialogWireframe description : application.getDialogSource().getViewSource().getCurrentWorkingSet()) {
+            long id = description.getPeerId() * 2 + description.getPeerType();
+            if (founded.contains(id)) {
+                continue;
+            }
+            if (matcher.isMatched(description.getDialogTitle())) {
+                founded.add(id);
+                SpannableString spannableString = new SpannableString(description.getDialogTitle());
+                if (description.getPeerType() == PeerType.PEER_USER) {
+                    User usr = application.getEngine().getUser(description.getPeerId());
+                    wireframes.add(new SearchWireframe(description.getPeerId(), description.getPeerType(), description.getDialogTitle(), spannableString, description.getDialogAvatar(), usr.getStatus(), 0));
+                } else {
+                    wireframes.add(new SearchWireframe(description.getPeerId(), description.getPeerType(), description.getDialogTitle(), spannableString, description.getDialogAvatar(), null, 0));
+                }
+            }
+        }
 
         for (DialogDescription description : descriptions) {
             long id = description.getPeerId() * 2 + description.getPeerType();
@@ -66,19 +68,20 @@ public class SearchKernel {
             }
         }
 
-//        User[] contacts = application.getContactsSource().getSortedUsers();
-//
-//        for (User u : contacts) {
-//            long id = u.getUid() * 2 + PeerType.PEER_USER;
-//            if (founded.contains(id)) {
-//                continue;
-//            }
-//            if (matcher.isMatched(u.getDisplayName())) {
-//                founded.add(id);
-//                SpannableString spannableString = new SpannableString(u.getDisplayName());
-//                wireframes.add(new SearchWireframe(u.getUid(), PeerType.PEER_USER, u.getDisplayName(), spannableString, u.getPhoto(), u.getStatus(), 0));
-//            }
-//        }
+        ContactsSource.LocalContact[] contacts = application.getContactsSource().getTelegramContacts();
+
+        for (ContactsSource.LocalContact c : contacts) {
+            User u = c.user;
+            long id = u.getUid() * 2 + PeerType.PEER_USER;
+            if (founded.contains(id)) {
+                continue;
+            }
+            if (matcher.isMatched(u.getDisplayName())) {
+                founded.add(id);
+                SpannableString spannableString = new SpannableString(u.getDisplayName());
+                wireframes.add(new SearchWireframe(u.getUid(), PeerType.PEER_USER, u.getDisplayName(), spannableString, u.getPhoto(), u.getStatus(), 0));
+            }
+        }
 
         return wireframes.toArray(new SearchWireframe[0]);
     }
