0087b90a0d365c370586af78160947cb68f2fde5
Prateek Srivastava <f2prateek@gmail.com>
2014-02-25 14:02:08 -0700

    Delegate copy(File, File) to copy(InputStream, File)

--- app/src/main/java/org/telegram/android/util/IOUtils.java
+++ app/src/main/java/org/telegram/android/util/IOUtils.java
@@ -14,16 +14,8 @@ public class IOUtils {
 
     public static void copy(File src, File dst) throws IOException {
         InputStream in = new FileInputStream(src);
-        OutputStream out = new FileOutputStream(dst);
-
-        // Transfer bytes from in to out
-        byte[] buf = new byte[1024];
-        int len;
-        while ((len = in.read(buf)) > 0) {
-            out.write(buf, 0, len);
-        }
+        copy(in, dst);
         in.close();
-        out.close();
     }
 
     public static void copy(InputStream in, File dst) throws IOException {
